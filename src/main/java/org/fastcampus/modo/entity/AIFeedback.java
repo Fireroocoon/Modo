b8379663d699eb42fb73b1837bb387e62cfedb24
kimsee <ttungseui11@gmail.com>
2025-06-06 15:39:25 +0900

    sfd

--- src/main/java/org/fastcampus/modo/entity/AIFeedback.java
+++ src/main/java/org/fastcampus/modo/entity/AIFeedback.java
@@ -1,5 +1,37 @@
 package org.fastcampus.modo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ai_feedback")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AIFeedback {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer feedbackId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 50, nullable = true)
+    private String topic;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String content;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String content2;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String method;
+
+    @Column(nullable = true)
+    private LocalDateTime createdAt;
 }
